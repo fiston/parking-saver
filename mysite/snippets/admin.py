98efed889ec4e51da30fedae264acc62792c0ee1
jyotman94 <jyotman94@gmail.com>
2016-04-21 13:21:04 +0530

    Location model added to admin

--- mysite/snippets/admin.py
+++ mysite/snippets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+class LocationAdmin(admin.ModelAdmin):
+	list_display = ('owner', 'address', 'created', 'retrieved')
+
+admin.site.register(Location, LocationAdmin)
